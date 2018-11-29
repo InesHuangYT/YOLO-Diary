733d1192edfc35dd807efc03d210fdfd00e224d1
mii860417 <34264872+mii860417@users.noreply.github.com>
2018-11-29 19:29:21 +0800

    1128

--- src/ListPic.js
+++ src/ListPic.js
@@ -34,41 +34,6 @@ componentDidMount() {
    }
 
 
-<<<<<<< HEAD
-// sendData_button_Next_to_listData1 = () => {
-//   const dataSheet = this.props.appActions.getDataSheet('listDataPic');
-
-//   let row = this.props.dataSheetRow || {
-//   };
-//   row = {
-//     ...row,
-//     photo: this.state.src,
-//   };
-//   if (this.props.dataSheetId === dataSheet.id) {
-//     this.props.appActions.updateInDataSheet('listDataPic', row);
-//   } else {
-//     this.props.appActions.addToDataSheet('listDataPic', row);
-//   }
-// }
-
-  componentDidMount() {
-    
-    console.log('listPic check props ->',this.props)
-    let _this = this;
-    // this.config = {
-    //   headers:{
-    //   'Authorization':'Bearer '+ sessionStorage.getItem('accesstoken')
-    //   }
-      
-    // };
-      axios.get(this.props.dataSheetRow.photoId).then(res=>{
-        console.log(this.props.dataSheetRow.photoId)
-      console.log('photoId', res)
-      _this.setState({src:res.data.photodata})
-      // _this.setState({src:Buffer.from(res.data, 'binary').toString('base64')})
-      //console.log('buffer=>', _this.state.src)
-    })
-=======
 sendData_button_Next_to_listData1 = () => {
   const dataSheet = this.props.appActions.getDataSheet('listDataPic');
 
@@ -86,7 +51,6 @@ sendData_button_Next_to_listData1 = () => {
 
 
   
->>>>>>> bd4dd49bc6abbd437bbd019ce4864798d1cf7ebe
     
     
     
