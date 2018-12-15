f3062ed94b8aaa192842f09bccf18adbc4d11a0b
Rrou03 <qq649492@gail.com>
2018-12-15 13:38:54 +0800

    save

--- src/FaceList.js
+++ src/FaceList.js
@@ -41,7 +41,7 @@ export default class FaceList extends Component {
         height: 'auto',
      };
     const style_roundimage_outer = {
-        pointerEvents: 'none',
+        pointerEvents: '',
      };
     
     return (
@@ -50,7 +50,7 @@ export default class FaceList extends Component {
           <div className='elRoundimage' style={style_roundimage_outer}>
           {/*(要做的事情) hover至人臉圖上顯示使用者名稱 https://bootstrap.hexschool.com/docs/4.1/components/tooltips/ */}
             <img   style={style_roundimage} src={"data:image/jpeg;base64, " + this.state.src} alt=""  />
-            
+            <span className="tooltiptext">路人甲</span>
           </div>
           
         </div>
