f64860c454d86800a09574cac21b21d72dbf0398
Rrou03 <qq649492@gail.com>
2018-12-16 22:01:26 +0800

    找不到用戶時顯示視窗

--- src/FaceList.js
+++ src/FaceList.js
@@ -4,7 +4,8 @@ import img_elRoundimage from './images/ListItem2_elUserPic_268846.png';
 import NewFaceRecScreen from './NewFaceRecScreen.js'
 import Input from 'muicss/lib/react/input';
 import { Button } from 'antd';
-import { Modal, message } from 'antd';
+import { Modal, message,notification } from 'antd';
+import 'antd/dist/antd.css';
 import img_deleteIcon from './images/deleteIcon.png'
 import axios from 'axios';
 
@@ -12,18 +13,23 @@ export default class FaceList extends Component {
 
   // This component doesn't use any properties
 
+
   constructor(props) {
     super(props);
 
     this.state = {
       src: '',
       user: '',
-      hidden: true
+      hidden: true,
+      value:'',
+      emailNotFound_visible: false,
+      usernameNotFound_visible: false
+
     };
 
   }
 
-
+  
 
   componentDidMount() {
 
@@ -46,35 +52,48 @@ export default class FaceList extends Component {
 
 
   textInputChanged_field = (event) => {
-    
+
   }
 
-  keyPress(e) {
+
+  // https://reactjs.org/docs/handling-events.html(看第6張圖)
+  // 
+  keyPress = (e) => {
     if (e.keyCode == 13) {
+
       console.log('value', e.target.value);
+      this.setState({value : e.target.value}) 
       var value = e.target.value
       if (value.indexOf("@") >= 0) {
         axios.get(`/api/user/checkEmailAvailability?email=${value}`).then(res => {
           console.log('get: ', res)
-          if(res.data.available == true){
+          if (res.data.available == true) {
             //找到此用戶的email，直接標記
-            message.success('已將此照片標記為'+ res.data.username)
-          }else{
+            message.success('已將此照片標記為' + res.data.username)
+          } else {
             //此使用者尚未註冊，是否發送邀請?
             //showModel
-          }
-          
-         
+            this.show_emailNotFound_Modal()
 
-        }).catch(function(error) {
+
+          }
+
+
+
+        }).catch(function (error) {
           console.log(error)
         });
 
 
-      }else{
+      } else {
         //用使用者名稱尋找資料庫有無此使用者
         axios.get(`/api/user/checkUsernameAvailability?username=${value}`).then(res => {
           console.log('Check username', res)
+          if(res.data.available == true){
+
+          }else{
+            this.show_usernameNotFound_Modal()
+          }
         })
 
       }
@@ -83,12 +102,60 @@ export default class FaceList extends Component {
 
     }
 
-   
+
+  }
+
+  show_emailNotFound_Modal = () => {
+    this.setState({
+      emailNotFound_visible: true,
+    });
+  }
+
+  emailNotFound_handleOk = (e) => {
+
+
+    this.setState({
+      emailNotFound_visible: false,
+    });
+  }
+
+  emailNotFound_handleCancel = (e) => {
+
+    this.setState({
+      emailNotFound_visible: false,
+    });
   }
 
 
+  show_usernameNotFound_Modal = () => {
+    this.setState({
+      usernameNotFound_visible: true,
+    });
+  }
+
+  usernameNotFound_handleOk = (e) => {
+
+
+    this.setState({
+      usernameNotFound_visible: false,
+    });
+  }
+
+  usernameNotFound_handleCancel = (e) => {
+
+    this.setState({
+      usernameNotFound_visible: false,
+    });
+  }
+
 
   render() {
+    const openNotification = () => {
+      notification.open({
+        message: 'Notification Title',
+        description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+      });
+    };
 
     var shown = {
       display: this.state.hidden ? "block" : "none"
@@ -105,6 +172,9 @@ export default class FaceList extends Component {
     let layoutFlowStyle = {};
 
 
+    const textStyle = {
+      textAlign: 'center',
+    }
 
     const style_roundimage = {
       height: 'auto',
@@ -125,29 +195,65 @@ export default class FaceList extends Component {
           https://codepen.io/joshbivens/pen/LNLKor
            */}
             <div style={shown}> </div>
-            <Input className="tagfield" type="text" style={hidden} onChange={this.textInputChanged_field} onKeyDown={this.keyPress} ></Input>
-            <img style={style_roundimage} src={"data:image/jpeg;base64, " + this.state.src} alt="" onClick={this.img_Onclick.bind(this)} />
-            <span className="tooltiptext">{this.state.user}</span>
+            <Input className="tagfield" type="text" style={hidden} onChange={this.textInputChanged_field} onKeyDown={this.keyPress} >
+            </Input>
 
-            <div className='img_deleteIcon' style={style_img_deleteIcon}>
-              <img style={style_img_deleteIcon} src={img_deleteIcon} />
-            </div>
+            <Modal title="找不到此信箱" visible={this.state.emailNotFound_visible} onOk={this.emailNotFound_handleOk} onCancel={this.emailNotFound_handleCancel} width='350px'>
+              <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+              <div style={textStyle} >
+                此用戶尚未註冊，是否發送邀請到此email?
+         </div>
+              <div className="w3-row w3-section">
+                <div className="w3-col" style={{width:'50px'}}><i className="w3-xxlarge fa fa-envelope-o"></i></div>
+                <div className="w3-rest">
+                  <input className="w3-input w3-border" name="email" type="text" placeholder={this.state.value} defaultValue={this.state.value}/>
+                </div>
+                </div>
+               
+            </Modal>
+
+              <Modal title="找不到此用戶" visible={this.state.usernameNotFound_visible} onOk={this.usernameNotFound_handleOk} onCancel={this.usernameNotFound_handleCancel} width='350px'>
+              <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                <div style={textStyle} >
+                  此用戶尚未註冊<br />
+                  在下方輸入好友的email即可邀請對方來使用YOLO日記!
+         </div>
+         <div className="w3-row w3-section">
+                <div className="w3-col" style={{width:'50px'}}><i className="w3-xxlarge fa fa-envelope-o"></i></div>
+                <div className="w3-rest">
+                  <input className="w3-input w3-border" name="email" type="text" placeholder="Enter Email" />
+                </div>
+                </div>
+
+              </Modal>
+
+              <img style={style_roundimage} src={"data:image/jpeg;base64, " + this.state.src} alt="" onClick={this.img_Onclick.bind(this)} />
+              <span className="tooltiptext">{this.state.user}</span>
+
+              <div className='img_deleteIcon' style={style_img_deleteIcon}>
+                <img style={style_img_deleteIcon} src={img_deleteIcon} />
+              </div>
 
           </div>
 
 
 
 
-        </div>
+
+          </div>
 
 
 
-      </div >
-    )
-  }
 
 
-}
+        </div >
+        )
+      }
+    
+    
+    }
 // const ShowTextField = () =>{
 //   <Input type="text"/>
 // }
